bbc43a1c37df29106a4ceca8012779e4d1163462
Egorvah <scsware@yandex.ru>
2012-12-20 09:45:45 +0400

    translete label

--- form/FormAdditionalFieldsBehavior.php
+++ form/FormAdditionalFieldsBehavior.php
@@ -19,7 +19,7 @@ class FormAdditionalFieldsBehavior extends CBehavior implements IFormMixinBehavi
             echo "<fieldset>";
             foreach ($form->model->additionalFields() as $k => $v) { ?>
             <div class="row">
-                <?php echo $form->labelEx($form->model, $k); ?>
+                <?php echo $form->labelEx($form->model, Yii::t('app', $k)); ?>
                 <?php echo $form->$v($form->model, $k); ?>
                 <?php echo $form->error($form->model, $k); ?>
             </div>
